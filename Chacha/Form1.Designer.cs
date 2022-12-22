8350330bac27aeb61b9e52b72a439ab824cf1e4d
PIBO0121030 Гуляев Владислав Егорович <PIBO0121030@sibupk.su>
2022-12-22 17:24:51 +0700

    Измененно

--- Chacha/Form1.Designer.cs
+++ Chacha/Form1.Designer.cs
@@ -36,6 +36,17 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.SuspendLayout();
             // 
             // listBox1
@@ -43,13 +54,13 @@
             this.listBox1.FormattingEnabled = true;
             this.listBox1.Location = new System.Drawing.Point(12, 12);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(166, 342);
+            this.listBox1.Size = new System.Drawing.Size(245, 420);
             this.listBox1.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(181, 8);
+            this.label1.Location = new System.Drawing.Point(280, 90);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(89, 13);
             this.label1.TabIndex = 1;
@@ -57,7 +68,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(184, 24);
+            this.textBox1.Location = new System.Drawing.Point(375, 90);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 2;
@@ -65,7 +76,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(184, 308);
+            this.button1.Location = new System.Drawing.Point(263, 334);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 46);
             this.button1.TabIndex = 3;
@@ -75,7 +86,7 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(184, 63);
+            this.textBox2.Location = new System.Drawing.Point(375, 122);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 4;
@@ -83,7 +94,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(181, 47);
+            this.label2.Location = new System.Drawing.Point(321, 122);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(48, 13);
             this.label2.TabIndex = 5;
@@ -92,7 +103,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(181, 86);
+            this.label3.Location = new System.Drawing.Point(327, 157);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(42, 13);
             this.label3.TabIndex = 6;
@@ -100,16 +111,118 @@
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(184, 102);
+            this.textBox3.Location = new System.Drawing.Point(375, 157);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(100, 20);
             this.textBox3.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(263, 386);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 46);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Очистить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(338, 196);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Сорт";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(375, 196);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(568, 203);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Дата поставки";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(487, 90);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(165, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Признак поставки по договору";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(658, 90);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 14;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(658, 125);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 15;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(658, 164);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 20);
+            this.textBox8.TabIndex = 16;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(585, 125);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(66, 13);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Количество";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(587, 164);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(65, 13);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Поставщик";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(658, 203);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(132, 20);
+            this.dateTimePicker1.TabIndex = 19;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -135,6 +248,17 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
     }
 }
 
--- Chacha/Form1.cs
+++ Chacha/Form1.cs
@@ -23,21 +23,91 @@ namespace Chacha
         }
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            if(textBox1.TextLength == 1)  
-            {
-                textBox1.Text = " --- ";
-            }
+
         }
 
 
         private void button1_Click(object sender, EventArgs e)
         {
-            listBox1.Items.Add("Наименнование: " + textBox1.Text);
-            listBox1.Items.Add("Артикул: " + textBox2.Text);
-            listBox1.Items.Add("Группа: " + textBox3.Text);
+            if (textBox1.TextLength == 0)
+            {
+                listBox1.Items.Add("Наименнование: --- ");
+            }
+            else
+            {
+                listBox1.Items.Add("Наименнование: " + textBox1.Text);
+            }
+            if (textBox2.TextLength == 0)
+            {
+                listBox1.Items.Add("Артикул: --- ");
+            }
+            else
+            {
+                listBox1.Items.Add("Артикул: " + textBox2.Text);
+            }
+            if (textBox3.TextLength == 0)
+            {
+                listBox1.Items.Add("Группа: --- ");
+            }
+            else
+            {
+                listBox1.Items.Add("Группа: " + textBox3.Text);
+            }
+            if (textBox4.TextLength == 0)
+            {
+                listBox1.Items.Add("Сорт: --- ");
+            }
+            else
+            {
+                //if()
+                listBox1.Items.Add("Сорт: " + textBox4.Text);
+            }
+            listBox1.Items.Add("Дата поставки: " + dateTimePicker1.Text);
+            if (textBox6.TextLength == 0)
+            {
+                listBox1.Items.Add("Признак поставки по договору: --- ");
+            }
+            else
+            {
+                //if()
+                listBox1.Items.Add("Признак поставки по договору: " + textBox6.Text);
+            }
+            if (textBox7.TextLength == 0)
+            {
+                listBox1.Items.Add("Количество: --- шт.");
+            }
+            else
+            {
+                //if()
+                listBox1.Items.Add("Количество: " + textBox7.Text + " шт.");
+            }
+            if (textBox8.TextLength == 0)
+            {
+                listBox1.Items.Add("Поставщик: --- ");
+            }
+            else
+            {
+                //if()
+                listBox1.Items.Add("Поставщик: " + textBox8.Text);
+            }
+            listBox1.Items.Add("  ----------------------------------------------");
             textBox1.Clear();
             textBox2.Clear();
             textBox3.Clear();
+            textBox4.Clear();
+            textBox6.Clear();
+            textBox7.Clear();
+            textBox8.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
